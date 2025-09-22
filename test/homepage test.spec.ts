97b37ac1b672873810f6b43f7a9c441ef0e8158d
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-21 21:13:58 -0500

    added test

--- test/homepage test.spec.ts
+++ test/homepage test.spec.ts
@@ -4,4 +4,6 @@ test("Home Page title validation", async ({ page }) => {
   let homePage = new HomePage(page);
   await page.goto("https://www.cars.com");
   await homePage.validateTitle("Imagine the possibilities");
+  await homePage.giveDetailsForSearch("audi", "audi-a8", "40", "60062");
+  await page.waitForTimeout(10_000);
 });
